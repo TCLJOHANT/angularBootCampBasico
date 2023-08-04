e2cc8d23d22124dad18fa89736bc6483f4525a56
johanChte <jachate7qmisena.edu.co>
2023-08-04 10:14:16 -0500

    mandando desde servicios promesas y observable, asi como en componente uso la promesa y observable, ademas de desuscribirme con Ondestroy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { SaludarComponent } from './components/saludar/saludar.component';
 import { FormsModule } from '@angular/forms';
@@ -16,7 +16,8 @@ import { ListaContactoComponent } from './components/lista-contacto/lista-contac
   imports: [
     BrowserModule,
     FormsModule,
-    ListsModule //importo mi propio modulo
+    ListsModule, //importo mi propio modulo
+    HttpClientModule, //para hacer peticiones http
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/contacto.service.ts
+++ src/app/services/contacto.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
 import { CONTACTOS } from '../mocks/contactos.mock';
 import { Contacto } from '../models/contacto.interface';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ContactoService {
 
   constructor() { }
-  obtenerContacto():Contacto []{
-    return CONTACTOS;
-  }
-  obtenerContactoPorID(id:number):Contacto|undefined{
+  //usando promesa :  se resuelve con una matriz de objetos de tipo  Contacto
+  obtenerContacto():Promise<Contacto []> {
+    return Promise.resolve(CONTACTOS);
+  } 
+  //usando Observable
+  obtenerContactoPorID(id:number):Observable<Contacto>|undefined {
+    //buscardo contacto por id dentro de la lista en mockeados
     const contacto = CONTACTOS.find((contacto:Contacto)=>contacto.id===id);
+    //creacion de observable
+    let observable:Observable<Contacto> = new Observable(observer =>{
+      observer.next(contacto); //emite valor a todo componente subsrito
+      observer.complete()// ya no emite mas valores
+    })
     if(contacto){
-      return contacto;
+      return observable;
     }else{
       return;
     }
