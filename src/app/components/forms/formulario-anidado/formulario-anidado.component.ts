b83d5b6777f04fb05913af9c741537976888dda7
johanChte <jachate7qmisena.edu.co>
2023-08-09 15:02:44 -0500

    formulario reactivo anidado

--- src/app/components/forms/formulario-anidado/formulario-anidado.component.ts
+++ src/app/components/forms/formulario-anidado/formulario-anidado.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup } from '@angular/forms'; 
 @Component({
   selector: 'app-formulario-anidado',
   templateUrl: './formulario-anidado.component.html',
   styleUrls: ['./formulario-anidado.component.scss']
 })
-export class FormularioAnidadoComponent {
-
+export class FormularioAnidadoComponent implements OnInit {
+  //definicion de formulario
+  miFormularioAnidado:FormGroup = new FormGroup({});
+  constructor(private formBuilder:FormBuilder){}
+  ngOnInit(): void {
+    //agrupacion para telefono
+    const telefonoFijo =this.formBuilder.group( {
+      prefijo:'',
+      numero:''
+    })
+    const telefonoMovil = this.formBuilder.group({
+      prefijo:'',
+      numero:''
+    })
+    //agrupacion de formulario que ontiene dos agrupaciones
+    this.miFormularioAnidado = this.formBuilder.group({
+      telefonoFijo:telefonoFijo,
+      telefonoMovil:telefonoMovil
+    })
+  }
 }
